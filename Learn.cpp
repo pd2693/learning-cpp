22d1a4f58e8e0bdeb50e1ed17be617fb4be344d0
pd2693 <dwivedipriyanshu2693@gmail.com>
2024-01-23 21:12:30 +0530

    Add this new Project.

---
+++ Learn.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  // Hello World! This is my first program in C++
+
+  cout << "Hare Krishna!" << endl;
+  cout << "All glories to Srila Prabhupada." << '\n';
+  cout << "All glories to Sri Guru and Sri Gauranga.";
+  return 0;
+}
